d0db29d30cf5f8d741c82de7bac886b000c994dc
NicoBatti <nicolasbattias@gmail.com>
2024-09-13 14:33:32 -0300

    Front de juguete battigol
    YA ESTA CCONECTADO EL BAACK CON EL FRONT, PEDRO TIENE QUE HACER ESTE FROONT PERO BIEN.

--- backend/guardadoDePublicaciones.js
+++ backend/guardadoDePublicaciones.js
@@ -1,9 +1,9 @@
 // /* atributos del objeto van a ser: nombre, ft, materia, descripcion, año(3ro), nombre de usuario(se hace en el login),
 // mail del vendedor, telefono */
 import fs from "fs";
-import { start } from "repl";
 import {onEvent,startServer} from "soquetic";
 onEvent("publicar", publicar)
+
 function publicar(info){
     let numerito;
     let leido = fs.readFileSync("DATOS/publisSubidas.json","utf-8");
@@ -13,24 +13,6 @@ function publicar(info){
     }else{
         numerito= leido[leido.length-1].id + 1;
     }
-<<<<<<< Updated upstream
-
-    let DATOS = {
-        nombre: nombre,
-        foto: foto,
-        materia: materia,
-        descripcion: descripcion,
-        año:año,
-        nombreDeUsuario: nombreDeUsuario,
-        mail: mail,
-        numero: numero,
-        id: numerito
-    }
-    let elJson= fs.readFileSync("DATOS/publisSubidas.json","utf-8");
-    elJson=JSON.parse(elJson);
-    elJson.push(DATOS);
-    fs.writeFileSync("DATOS/publisSubidas.json",JSON.stringify(elJson));
-=======
     let nombre = info.nombre;
     let foto = info.foto;
     let materia = info.materia;
@@ -39,13 +21,22 @@ function publicar(info){
     let nombreDeUsuario = info.nombreDeUsuario;
     let mail = info.mail;
     let numero = info.numero;
-    numerito = info.id
-    let elJson= fs.readFileSync("../publisSubidas.json","utf-8");
-    elJson=JSON.parse(elJson);
-    elJson.push(datos);
-    fs.writeFileSync("../publisSubidas.json",JSON.stringify(elJson));
+
+    let datos = {
+        nombre:nombre,
+        foto:foto,
+        materia:materia,
+        descripcion:descripcion,
+        año:año,
+        nombreDeUsuario:nombreDeUsuario,
+        mail:mail,
+        numero:numero,
+        id:numerito
+    }
+
+    leido.push(datos);
+    fs.writeFileSync("DATOS/publisSubidas.json",JSON.stringify(leido));
     return true
->>>>>>> Stashed changes
 }
 
 //ELIMINAR PUBLICACIONES:
@@ -61,10 +52,5 @@ function eliminar(id){
     }
     fs.writeFileSync("DATOS/publisSubidas.json",JSON.stringify(nuevaLista));
 }
-<<<<<<< Updated upstream
 
-
-publicar("nombre","foto","materia","descripcion","año","nombreDeUsuario","mail","numero");
-=======
 startServer();
->>>>>>> Stashed changes
