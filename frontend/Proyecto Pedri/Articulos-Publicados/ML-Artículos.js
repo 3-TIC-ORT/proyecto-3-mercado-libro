82b1f08a3229dfa088be0c9c4eb13488a3b6fddf
Leoli16 <abudleito@gmail.com>
2024-10-23 10:56:30 -0300

    Imagenes subidas

--- frontend/Proyecto Pedri/Articulos-Publicados/ML-Artículos.js
+++ frontend/Proyecto Pedri/Articulos-Publicados/ML-Artículos.js
@@ -1,3 +1,4 @@
+let resultado = "";
 //////////////////////PARA VOLVER AL LOGIN SI NO SE ESTÁ REGISTRADO ///////////////////////////
 window.addEventListener("pageshow",()=>{
     window.reload()
@@ -30,7 +31,7 @@ window.onload = ()=>{
         let publicados = document.getElementById("publicadosinvisible");
         publicados.innerHTML = "";
         for (let i of listaDeLibros){
-            publicados.innerHTML = publicados.innerHTML + `<div class="libros"><div class="imagen"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShaWwHk25rOFdbzWhtU8iT7A1IIBJandpDVg&s" class="img" alt=""></div><div class="info"><h6 class="nombre">${i.nombre}</h6><h6 class="precio">${i.precio}</h6></div></div>`
+            publicados.innerHTML = publicados.innerHTML + `<div class="libros"><div class="imagen"><img src="${i.foto}" class="img" alt=""></div><div class="info"><h6 class="nombre">${i.nombre}</h6><h6 class="precio">${i.precio}</h6></div></div>`
         }
     })
 }
@@ -40,23 +41,22 @@ document.getElementById("botonPublicar").addEventListener("click", publicar);
 function publicar(){ 
     // TODO: PEDRO ACA ESTAN TODOS LOS IDS QUE TENES QUE PONER. Tambien tenes que agregar el boton publicar id: botonPublicar 
     let nombre = document.getElementById("nombre");
-    let imagen = document.getElementById("imagen");
     let materia = document.getElementById("materia");
     let precio = document.getElementById("precio");
     let descripcion = document.getElementById("descripcion");
     let año = document.getElementById("año");
     let nombreDeUsuario = localStorage.getItem("user");
     let mail = document.getElementById("mail");
-    let numero = document.getElementById("numero");// TODO: Agregar comparación de precio e imagen
-    if( nombre.value === "" || materia.value === "" || descripcion.value === "" || año.value === "" || mail.value === ""){
+    let numero = document.getElementById("numero");// TODO: Agregar comparación de precio
+    if( nombre.value === "" || materia.value === "" || descripcion.value === "" || año.value === "" || mail.value === "" || resultado === ""){
         alert("Es necesario completar todos los campos para poder publicar");
     }
     else{
         let publicacion = {
-            "nombre": nombre.value,
-            "imagen": "SIUUU", //TODO: Agregar link img
+            "nombre": nombre.value.charAt(0).toUpperCase() + nombre.value.slice(1).toLowerCase(),
+            "foto": resultado,
             "materia": materia.value,
-            "precio": "SIUUUU", //TODO: Agregar precio posta cuando esté en el css
+            "precio": "$"+"precio.value", //TODO: Agregar precio posta cuando esté en el css
             "descripcion": descripcion.value,
             "año": año.value,
             "nombreDeUsuario": localStorage.getItem("user"),
@@ -65,6 +65,7 @@ function publicar(){
         }
         modal.close()
         postData("publicar",publicacion);
+        resultado = "";
         window.reload()
     }
 }
@@ -92,9 +93,15 @@ function displayImageName(){
     var fileReader = new FileReader();
     fileReader.readAsDataURL(fileInput.files[0]);
     fileReader.onloadend = ()=>{
-        let resultado = FileReader.result;
+        resultado = fileReader.result;
+        var fileName = fileInput.files[0] ? fileInput.files[0].name : "Ninguna imagen seleccionada";
+        if (fileName === "Ninguna imagen seleccionada"){
+            document.getElementById('nombreImagen').textContent = fileName;
+        }else{
+            document.getElementById('nombreImagen').innerHTML = `<img id="ay" width="330px" src="${resultado}" alt=""></img>`
+        }
     }
 
-    var fileName = fileInput.files[0] ? fileInput.files[0].name : "Ninguna imagen seleccionada";
-    document.getElementById('nombreImagen').textContent = fileName;
+    
+    
 }
